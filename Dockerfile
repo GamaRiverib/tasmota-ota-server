aa68bb13129e049461c386fbd03569e00eb723f2
Jose Gamaliel Rivera Ibarra <jgrivera@novutek.com>
2019-12-26 20:13:39 -0700

    Proof of concept

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM node:10.17.0-alpine
+WORKDIR /usr/src/app
+RUN apk --update add git less openssh \
+        python python-dev py-pip build-base && \
+    pip install -U platformio && \
+    rm -rf /var/lib/apt/lists/* && \
+    rm /var/cache/apk/*
+COPY package*.json ./
+RUN npm i
+RUN npm i typescript -g
+COPY tsconfig.json ./
+COPY ./src ./src
+RUN npm run build
+RUN rm -rf ./node_modules
+RUN rm -rf ./src
+RUN npm uninstall typescript -g
+RUN rm tsconfig.json
+RUN npm i --only=prod
+RUN rm package.json
+RUN rm package-lock.json
+RUN mkdir content
+VOLUME [ "/usr/src/app/content" ]
+CMD ["node", "build/index.js"]
---
+++ src/index.ts
@@ -0,0 +1,74 @@
+import { join } from "path";
+import { exec, ExecException } from "child_process";
+
+import winston = require("winston");
+import { Request, Response, NextFunction, Application } from "express";
+import { getLogger } from "./logger";
+
+const repository: string = "https://github.com/arendst/Tasmota.git";
+const directory: string = join(__dirname, "../content/source");
+const content: string = join(__dirname, "../content");
+
+const express = require("express");
+const app: Application = express();
+
+const logger: winston.Logger = getLogger("[TASMOTA]");
+
+app.use("/tasmota/release", express.static(content));
+
+function run_command(command: string): Promise<string> {
+    return new Promise<string>((resolve, reject) => {
+        exec(command, (err: ExecException, stdout: string, stderr: string) => {
+            if(err !== null) {
+                return reject(err);
+            }
+            if(typeof stderr !== "string") {
+                return reject(stderr);
+            }
+            resolve(stdout);
+        });
+    });
+}
+
+app.get("/source/download", async (req: Request, res: Response, next: NextFunction) => {
+    logger.info("Download Tasmota source code");
+    const command: string = `git clone ${repository} ${directory}`;
+    logger.info("Running command " + `${command}`);
+    try {
+        const result: string = await run_command(command);
+        res.status(200).send(result || "Done!");
+    } catch (reason) {
+        res.status(400).send(reason);
+    }
+});
+
+app.get("/source/update", async (req: Request, res: Response, next: NextFunction) => {
+    logger.info("Update Tasmota source code");
+    const command: string = `cd ${directory} && git pull`;
+    try {
+        const result: string = await run_command(command);
+        res.status(200).send(result || "Done!");
+    } catch (reason) {
+        res.status(400).send(reason);
+    }
+});
+
+app.get("/source/build/:env", async (req: Request, res: Response, next: NextFunction) => {
+    logger.info("Compile Tasmota source code");
+    const env: string = req.params.env;
+    const command: string = `cd ${directory} && pio run --environment ${env}`;
+    try {
+        let result: string = await run_command(command);
+        logger.info("Moving binary file");
+        result += "\r\n";
+        const mv_cmnd: string = `cp ${join(directory, ".pioenvs", env, "firmware.bin")} ${join(content, env + ".bin")}`;
+        result += await run_command(mv_cmnd);
+        res.status(200).send(result);
+    } catch (reason) {
+        res.status(400).send(reason);
+    }
+});
+
+app.listen(8266, () => {
+    logger.info("Tasmota OTA Server is running on port 8266");
+});
